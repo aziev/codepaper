6068ce479b1166e0d057ec13db2f4c2817774aa9
mik <mikail.aziev@gmail.com>
2016-11-29 18:10:23 +0300

    write index frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <title>Particles</title>
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 </head>
 <body>
-    Hello
+    <nav class="mainnav">
+        <div class="container">
+            <a href="">Все статьи</a>
+            <a href="">Категории</a>
+            <a href="">О сайте</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="col-xs-12 col-md-8 col-md-offset-2">
+            <div class="row">
+                <article class="article">
+                    <a href="">
+                        <img src="http://placehold.it/500x300/ffd640" alt="">
+                    </a>
+                    <h2>Разработка на Laravel с нуля</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non quam blanditiis, modi commodi excepturi impedit laudantium consequatur tempora dolorem obcaecati quasi, sunt dolorum magni nisi possimus fuga! Non, dolores!</p>
+                </article>
+                <article class="article">
+                    <img src="http://placehold.it/500x300/09b5df" alt="">
+                    <h2>Разработка на Laravel с нуля</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio non quam blanditiis, modi commodi excepturi impedit laudantium consequatur tempora dolorem obcaecati quasi, sunt dolorum magni nisi possimus fuga! Non, dolores!</p>
+                </article>
+            </div>
+        </div>
+    </div>
+    <footer class="container">
+        <p>&copy; Particles 2017</p>
+    </footer>
 </body>
 </html>
