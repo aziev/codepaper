b66d54b6119046b0d8d39e98748a8f9fc905785a
teslaAi <storona77@gmail.com>
2016-11-29 14:00:56 +0300

    create category and article models

---
+++ database/migrations/2016_11_29_105230_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->text('text');
+            $table->integer('category_id')->unsigned();
+
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
