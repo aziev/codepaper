a020c27d6d880b45ebdc5e629d4528fdcf82b326
Mikail Aziev <mikail.aziev@gmail.com>
2016-12-05 14:11:02 +0300

    change class notation

--- app/Tag.php
+++ app/Tag.php
@@ -3,11 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Article;
 
 class Tag extends Model
 {
     public function articles()
     {
-    	return $this->belongsToMany('App\Article');
+    	return $this->belongsToMany(Article::class);
     }
 }
