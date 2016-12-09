23e475ce05be3c782a70b13bb30afe48c653b46a
mik <mikail.aziev@gmail.com>
2016-12-09 14:06:49 +0300

    add article info to article page

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,6 +6,15 @@
 
 <article class="single">
     <h1>{{ $article->title }}</h1>
+    <div class="info">
+        <span class="date">{{ $article->getDate() }}</span>
+        <span class="views">
+            <i class="fa fa-eye" aria-hidden="true"></i>{{ $article->views }}
+        </span>
+        <span class="comments">
+            <i class="fa fa-comments-o" aria-hidden="true"></i>20
+        </span>
+    </div>
     <div class="image">
         <img src="http://placehold.it/700x400" alt="">
     </div>
