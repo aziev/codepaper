cabdc607f76f61823c44414d5d29472cc0aab125
Mikail <mikail@ant.ms>
2017-01-21 21:48:32 +0300

    update design

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <title>@yield ('title') {{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}">
     @yield ('meta')
 </head>
 <body>
