5d9b01becf4b32108a55020e2a86a87dea8c6e11
mik <mikail.aziev@gmail.com>
2017-02-03 16:56:00 +0300

    fix svg fallback

--- public/js/all.js
+++ public/js/all.js
@@ -2,13 +2,17 @@ const ROOT = document.querySelector('meta[name=root]').getAttribute('content') +
 
 var $logo = document.querySelector('.logo');
 
-if (typeof SVGRect == "undefined")
+if (!supportsSvg())
 {
     var $image = document.createElement('img');
     $image.src = ROOT + 'img/logo.png';
 
     $logo.innerHTML = '';
     $logo.appendChild($image);
+}
+
+function supportsSvg() {
+    return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0")
 }
 
 //# sourceMappingURL=all.js.map
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,11 +2,15 @@ const ROOT = document.querySelector('meta[name=root]').getAttribute('content') +
 
 var $logo = document.querySelector('.logo');
 
-if (typeof SVGRect == "undefined")
+if (!supportsSvg())
 {
     var $image = document.createElement('img');
     $image.src = ROOT + 'img/logo.png';
 
     $logo.innerHTML = '';
     $logo.appendChild($image);
+}
+
+function supportsSvg() {
+    return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0")
 }
