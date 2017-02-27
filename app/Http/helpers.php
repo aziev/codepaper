bd357dfddcb5cf0c477b2060cab22fc30c2ef71b
Mik Aziev <mikail.aziev@gmail.com>
2017-02-27 17:20:57 +0300

    add comments to helpers.php

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,6 +1,16 @@
 <?php
 
-function version($path)
-{
-    return asset("$path?v=" . filemtime(public_path($path)));
+if (! function_exists('version')) {
+    /**
+     * Asset file with version parameter.
+     *
+     * @param  string  $key
+     * @return string
+     */
+    function version($path)
+    {
+        $version = filemtime(public_path($path));
+
+        return asset("$path?v=$version");
+    }
 }
