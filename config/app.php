bd1ef4d282b7703227b0c0b891c966b71cf4e1e7
Mikail <mikail@ant.ms>
2017-03-13 18:13:36 +0300

    upgrade laravel to 5.4

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Tinker\TinkerServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         BladeSvg\BladeSvgServiceProvider::class,
 
