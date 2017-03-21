c57b4ce8964c5d87cd2569dd777fe4c115a43e09
Mik Aziev <mikail.aziev@gmail.com>
2017-03-21 14:10:41 +0300

    connect to adsense

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -74,5 +74,12 @@
         ga('create', 'UA-90727834-1', 'auto');
         ga('send', 'pageview');
     </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-9007766459323085",
+            enable_page_level_ads: true
+        });
+    </script>
 </body>
 </html>
